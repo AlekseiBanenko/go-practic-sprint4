69fd80f409a6811901b10c56bed4c0bbb4b60e47
Aleksei Banenko <79374270054@yandex.ru>
2025-12-27 23:57:06 +0300

    исправил логи ошибок в DayActionInfo: log.Println вместо fmt.Println

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -3,6 +3,7 @@ package daysteps
 import (
 	"errors"
 	"fmt"
+	"log"
 	"strconv"
 	"strings"
 	"time"
@@ -48,14 +49,14 @@ func parsePackage(data string) (int, time.Duration, error) {
 func DayActionInfo(data string, weight, height float64) string {
 	steps, duration, err := parsePackage(data)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return ""
 	}
 
 	distance := float64(steps) * stepLength / mInKm
 	calories, err := spentcalories.WalkingSpentCalories(steps, weight, height, duration)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return ""
 	}
 
