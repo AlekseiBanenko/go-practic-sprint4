55035e0e3274b962b8822782c814708bd580e959
Aleksei Banenko <79374270054@yandex.ru>
2025-12-27 23:42:07 +0300

    исправил лишний пробел в DayActionInfo + вернул fmt.Println

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -59,6 +59,6 @@ func DayActionInfo(data string, weight, height float64) string {
 		return ""
 	}
 
-	return fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал. \n",
+	return fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n",
 		steps, distance, calories)
 }
